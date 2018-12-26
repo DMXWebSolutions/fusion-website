d2853d8ca4217705fffb9d79afd5819345f73d5b
Daniel <danieliver2011@gmail.com>
2018-12-26 15:27:55 -0200

    novas paginas

---
+++ app/db/db_home.php
@@ -0,0 +1,30 @@
+<?php
+    return array(
+        "servicos" => array(
+            // array(
+            //     "img"       => "alarm.png",
+            //     "title"     => "ALARMES AUTOMOTIVOS",
+            //     "content"   => "",
+            // ),
+            array(
+                "img"       => "rastreio.png",
+                "title"     => "RASTREAMENTO DE VEÍCULO",
+                "content"   => "",   
+            ),
+            array(
+                "img"       => "carga.png",
+                "title"     => "RASTREAMENTO DE CARGA",
+                "content"   => "",    
+            ),
+            array(
+                "img"       => "cftv.png",
+                "title"     => "CFTV - CIRCUITO FECHADO DE TV",
+                "content"   => "",
+            ),
+            array(
+                "img"       => "portao.png",
+                "title"     => "PORTÕES ELÉTRICOS E PORTARIA ELETRÔNICA",
+                "content"   => "",
+            )
+        )
+);
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
+
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('resources/views', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
+/*VARIAVEIS GLOBAIS*/
+$slides = require('app/db/db_slides.php');
+$container['view']['slides'] = $slides;
+
+/* Rotas que renderizam as páginas que estão em ./resources/views*/
+
+$app->get('/', function($request, $response) {
+    $data = require('app/db/db_home.php');
+    return $this->view->render($response, 'home.mobile.twig', [
+        'pageTitle' => 'Home',
+        'servicos' => $data["servicos"],
+    ]);
+})->setName('home');
+
+$app->get('/about', function($request, $response) {
+    return $this->view->render($response, 'about.mobile.twig');
+})->setName('about');
+
+$app->get('/services', function($request, $response) {
+    return $this->view->render($response, 'services.mobile.twig');
+})->setName('services');
+
+$app->get('/contact', function($request, $response) {
+    return $this->view->render($response, 'contact.mobile.twig');
+})->setName('contact');
+
+$app->post('/contact', function($request, $response) {
+    die('Contact');
+})->setName('contact');
+
+$app->run();
