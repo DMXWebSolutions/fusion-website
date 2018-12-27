e75c797da4622b9031715dbe5a09ee64ad6729f5
Daniel Oliveira <danieliver2011@gmail.com>
2018-12-27 15:37:28 -0200

    ok

--- app/db/db_slides2.php
+++ app/db/db_slides2.php
@@ -1,5 +1,4 @@
 <?php
-
 return array(
     array(
         "path"      => "01.png",
@@ -11,5 +10,24 @@ return array(
         "legend"    => "",
         "active"    => "",
     ),
-
+    array(
+        "path"      => "03.png",
+        "legend"    => "",
+        "active"    => "",
+    ),
+    array(
+        "path"      => "04.png",
+        "legend"    => "",
+        "active"    => "",
+    ),
+    array(
+        "path"      => "05.png",
+        "legend"    => "",
+        "active"    => "",
+    ),
+    array(
+        "path"      => "06.png",
+        "legend"    => "",
+        "active"    => "",
+    ),
 );
